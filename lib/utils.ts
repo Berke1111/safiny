438b5eb5d7f0699e18844def74c7257a24c1942c
Berke Osman <jokerberke5225@gmail.com>
2024-07-08 14:39:37 +0200

    Update dependencies and fix UI issues

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,6 @@
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
-
 import { twMerge } from "tailwind-merge";
 
 import { aspectRatioOptions } from "@/constants";
